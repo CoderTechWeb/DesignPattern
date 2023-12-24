6effff6f6e22f54c25a4d0deab97762e76c11bac
snarayanaswamy_pfpt <snarayanaswamy@proofpoint.com>
2023-12-25 00:27:11 +0530

    Initial commit

---
+++ src/main/java/org/design/singleton/Main.java
@@ -0,0 +1,12 @@
+package org.design.singleton;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Student student = Student.getInstance();
+        Student student1 = Student.getInstance();
+
+        System.out.println(student);
+        System.out.println(student1);
+    }
+}
