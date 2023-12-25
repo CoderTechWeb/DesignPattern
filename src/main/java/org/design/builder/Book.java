fb9f3bf5851eccf13f379c5afd878865a3439ee7
snarayanaswamy_pfpt <reach_suresh@outlook.com>
2023-12-25 11:51:13 +0530

    Builder pattern

---
+++ src/main/java/org/design/builder/Book.java
@@ -0,0 +1,84 @@
+package org.design.builder;
+
+import java.time.Year;
+
+public class Book {
+
+    private final String isbn;
+    private final String title;
+    private final String author;
+    private final Year published;
+    private final String description;
+
+    public Book(Builder builder) {
+        this.isbn = builder.isbn;
+        this.title = builder.title;
+        this.author = builder.author;
+        this.published = builder.published;
+        this.description = builder.description;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Year getPublished() {
+        return published;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", published=" + published +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private final String isbn;
+        private final String title;
+        private String author;
+        private Year published;
+        private String description;
+
+        public Builder(String isbn, String title) {
+            this.isbn = isbn;
+            this.title = title;
+        }
+
+        public Builder  author(String author) {
+            this.author = author;
+            return this;
+        }
+
+        public Builder published(Year published) {
+            this.published = published;
+            return this;
+        }
+
+        public Builder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Book build() {
+            return new Book(this);
+        }
+    }
+
+}
