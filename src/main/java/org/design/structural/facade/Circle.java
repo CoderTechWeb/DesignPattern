03e465944294e3d55eda4f865878ff992c7982ef
CoderTechWeb <reach_suresh@outlook.com>
2024-01-05 22:20:27 +0530

    Facade Design pattern

---
+++ src/main/java/org/design/structural/facade/Circle.java
@@ -0,0 +1,8 @@
+package org.design.structural.facade;
+
+public class Circle implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("Circle draw");
+    }
+}
---
+++ src/main/java/org/design/structural/facade/ShapeMaker.java
@@ -0,0 +1,23 @@
+package org.design.structural.facade;
+
+public class ShapeMaker {
+    public Shape circle;
+    public Shape square;
+    public Shape rectangle;
+
+    public ShapeMaker() {
+        circle = new Circle();
+        square = new Square();
+        rectangle = new Rectangle();
+    }
+
+    public void drawCircle(){
+        circle.draw();
+    }
+    public void drawRectangle(){
+        rectangle.draw();
+    }
+    public void drawSquare(){
+        square.draw();
+    }
+}
